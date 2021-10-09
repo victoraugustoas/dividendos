061c5643ca1b311186f86533dc2656925cbf3e21
Victor Augusto <victor.silva@atem.com.br>
2021-10-09 02:25:45 -0400

    Update

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const port = 3001;
 const axios = require("axios").default;
 const { store_dividend } = require("./index");
 
+emitter.setMaxListeners(0);
+
 const urlDB =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
     : "http://db:3000";
 
 app.get("/:ticker", async (req, res) => {
-  const ticker = req.params.ticker;
+  const ticker = String(req.params.ticker).toLowerCase();
 
   store_dividend(ticker);
 
